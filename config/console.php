b6dde088f7ce0e2dc6bcde6b4634274b31a9970e
Igor <i.rvachev@onrg.ru>
2025-08-05 12:51:50 +0200

    fix: remove debug/gii modules from production

--- config/console.php
+++ config/console.php
@@ -36,13 +36,13 @@ $config = [
     ],
     */
 ];
-
+/*
 if (YII_ENV_DEV) {
-/*    // configuration adjustments for 'dev' environment
+   // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-    ];*/
+    ];
     // configuration adjustments for 'dev' environment
     // requires version `2.1.21` of yii2-debug module
     $config['bootstrap'][] = 'debug';
@@ -51,6 +51,6 @@ if (YII_ENV_DEV) {
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
-}
+}*/
 
 return $config;
