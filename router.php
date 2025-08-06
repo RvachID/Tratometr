cd7565bfa27a845b00ba3eeec45701e3985c69eb
Igor <i.rvachev@onrg.ru>
2025-08-06 16:33:03 +0200

    reg with e-mail & pin

--- router.php
+++ router.php
@@ -1,10 +1,11 @@
 <?php
+// Абсолютный путь до папки web
 $webRoot = __DIR__ . '/web';
 
-// Если запрашивается существующий файл (css/js/png и т.п.) — отдаём напрямую
-if (is_file($webRoot . $_SERVER["REQUEST_URI"])) {
+// Если запрашивается реальный файл (css/js/img) — отдать напрямую
+if (is_file($webRoot . $_SERVER['REQUEST_URI'])) {
     return false;
 }
 
-// Всё остальное отдаём в index.php
+// Всё остальное — через index.php
 require $webRoot . '/index.php';
