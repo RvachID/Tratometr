e6c81693e3aae3f01da4811696658305ca86553e
Igor <i.rvachev@onrg.ru>
2025-08-06 16:36:23 +0200

    reg with e-mail & pin

--- views/site/index.php
+++ views/site/index.php
@@ -4,7 +4,9 @@ use yii\helpers\Html;
 $this->title = 'Тратометр';
 ?>
 <h1>Тратометр</h1>
+
 <p>
-    <a href="<?= \yii\helpers\Url::to(['signup']) ?>">Регистрация</a> |
-    <a href="<?= \yii\helpers\Url::to(['login']) ?>">Вход</a>
+    <a href="<?= \yii\helpers\Url::to(['auth/signup']) ?>">Регистрация</a> |
+    <a href="<?= \yii\helpers\Url::to(['auth/login']) ?>">Вход</a>
 </p>
+
