cb6c4793111dd9dac13caffc5306daaa665b6a82
Igor <i.rvachev@onrg.ru>
2025-08-20 16:16:07 +0200

    Обновил Чейнджлог

--- config/params.php
+++ config/params.php
@@ -8,7 +8,7 @@ if (is_file($secretsFile)) {
 
 return [
     'appName' => 'Тратометр',
-    'version' => '0.5.0',
+    'version' => '0.6.0',
 
     'ocr' => [
         'endpoint' => getenv('OCR_ENDPOINT') ?: ($localSecrets['OCR_ENDPOINT'] ?? 'https://api.ocr.space/parse/image'),
