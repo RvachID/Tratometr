92504f2077e906cfaf074e68403bcd705548821c
Igor <i.rvachev@onrg.ru>
2025-08-29 11:53:57 +0200

    Продолжаю мучать распознование

--- config/params.php
+++ config/params.php
@@ -8,7 +8,7 @@ if (is_file($secretsFile)) {
 
 return [
     'appName' => 'Тратометр',
-    'version' => '0.7.1',
+    'version' => '0.7.3',
 
     'ocr' => [
         'endpoint' => getenv('OCR_ENDPOINT') ?: ($localSecrets['OCR_ENDPOINT'] ?? 'https://api.ocr.space/parse/image'),
