fdd5266af3c6e9070ef8ce7227d17e51738a4724
Igor <i.rvachev@onrg.ru>
2026-07-23 10:14:01 +0200

    вынес конфиги бд в переменные

--- config/db.php
+++ config/db.php
@@ -2,10 +2,16 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=yamabiko.proxy.rlwy.net;port=52403;dbname=railway',
-    'username' => 'root',
-    'password' => 'KIJvUsUrAdWXrrPCNNKHpNMXzYLIwpTS',
+    'dsn' => sprintf(
+        'mysql:host=%s;port=%s;dbname=%s',
+        getenv('DB_HOST') ?: 'localhost',
+        getenv('DB_PORT') ?: '3306',
+        getenv('DB_NAME') ?: 'tratometr'
+    ),
+    'username' => getenv('DB_USER') ?: 'tratometr',
+    'password' => getenv('DB_PASS') ?: '',
     'charset' => 'utf8mb4',
+
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
